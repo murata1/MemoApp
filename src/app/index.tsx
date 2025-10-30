ebe93e4fc02ca89afdfcd7c619216f250007f372
k.murata <90595844+murata1@users.noreply.github.com>
2025-10-30 23:34:34 +0900

    Markup MemoList

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,65 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Index = () => {
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年11月25日 14:00</Text>
+          </View>
+          <View>X</View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年11月25日 14:00</Text>
+          </View>
+          <View>X</View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年11月25日 14:00</Text>
+          </View>
+          <View>X</View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年11月25日 14:00</Text>
+          </View>
+          <View>X</View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
 
 export default Index;
