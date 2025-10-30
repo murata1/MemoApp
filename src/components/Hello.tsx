4972f0791a2af1acd82b7ef00d3d551f3026c863
k.murata <90595844+murata1@users.noreply.github.com>
2025-10-30 18:08:42 +0900

    create hello components

---
+++ src/components/Hello.tsx
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet, type TextStyle } from 'react-native';
+
+interface Props {
+  children: string;
+  bang?: boolean;
+  style?: TextStyle;
+}
+
+const Hello = (props: Props) => {
+  const { children, bang, style } = props;
+  return (
+    <View>
+      <Text style={[styles.text, style]}>
+        Hello {children}{bang === true ? '!' : ''}
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    padding: 16,
+  }
+});
+
+
+
+export default Hello;
